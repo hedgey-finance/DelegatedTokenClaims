7af3e48f0780d1433bb6e200442d8aa8c8ced121
iceman <acfmichelsen@gmail.com>
2024-05-08 16:22:10 -0400

    retest everything

--- test/test.js
+++ test/test.js
@@ -9,112 +9,112 @@ const { multiClaimTests } = require('./tests/multiClaimTests');
 const flashAttackTests = require('./tests/flashAttackTests');
 
 
-// const paramsMatrix = [
-//   {
-//     decimals: 18,
-//     totalRecipients: 100,
-//     nodeA: 0,
-//     nodeB: 1,
-//     nodeC: 2,
-//     nodeD: 8,
-//     nodeE: 15,
-//   },
-//   // {
-//   //   decimals: 6,
-//   //   totalRecipients: 100,
-//   //   nodeA: 0,
-//   //   nodeB: 1,
-//   //   nodeC: 2,
-//   //   nodeD: 8,
-//   //   nodeE: 10,
-//   // }
-// ];
+const paramsMatrix = [
+  {
+    decimals: 18,
+    totalRecipients: 100,
+    nodeA: 0,
+    nodeB: 1,
+    nodeC: 2,
+    nodeD: 8,
+    nodeE: 15,
+  },
+  {
+    decimals: 6,
+    totalRecipients: 100,
+    nodeA: 0,
+    nodeB: 1,
+    nodeC: 2,
+    nodeD: 8,
+    nodeE: 10,
+  }
+];
 
-// const lockupParamsMatrix = [
-//   {
-//     start: 1,
-//     cliff: 0,
-//     period: 1,
-//     periods: (60 * 60 * 24 * 30),
-//   },
-//   // {
-//   //   start: 1,
-//   //   cliff: 0,
-//   //   period: C.DAY,
-//   //   periods: (30),
-//   // },
-//   // {
-//   //   start: 0,
-//   //   cliff: 0,
-//   //   period: 1,
-//   //   periods: (60 * 60 * 24 * 30),
-//   // },
-//   // {
-//   //   start: 1,
-//   //   cliff: 0,
-//   //   period: C.MONTH,
-//   //   periods: (12),
-//   // }
-// ]
+const lockupParamsMatrix = [
+  {
+    start: 1,
+    cliff: 0,
+    period: 1,
+    periods: (60 * 60 * 24 * 30),
+  },
+  {
+    start: 1,
+    cliff: 0,
+    period: C.DAY,
+    periods: (30),
+  },
+  {
+    start: 0,
+    cliff: 0,
+    period: 1,
+    periods: (60 * 60 * 24 * 30),
+  },
+  {
+    start: 1,
+    cliff: 0,
+    period: C.MONTH,
+    periods: (12),
+  }
+]
 
-// describe('Testing the unlocked tests with delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     unlockedDelegatingTests(params);
-//   });
-//     unlockedDelegatingErrorTests();
-// });
+describe('Testing the unlocked tests with delegation', () => {
+  paramsMatrix.forEach((params) => {
+    unlockedDelegatingTests(params);
+  });
+    unlockedDelegatingErrorTests();
+});
 
-// describe('Testing the locked tests with delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     lockupParamsMatrix.forEach((lockupParams) => {
-//       lockedDelegatingTests(params, lockupParams);
-//     });
-//   });
-//   lockedDelegatingErrorTests();
-// });
+describe('Testing the locked tests with delegation', () => {
+  paramsMatrix.forEach((params) => {
+    lockupParamsMatrix.forEach((lockupParams) => {
+      lockedDelegatingTests(params, lockupParams);
+    });
+  });
+  lockedDelegatingErrorTests();
+});
 
-// describe('Testing the vesting tests with delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     lockupParamsMatrix.forEach((lockupParams) => {
-//       vestingDelegatingTests(params, lockupParams);
-//     });
-//   });
-//   vestingDelegatingErrorTests();
-// });
+describe('Testing the vesting tests with delegation', () => {
+  paramsMatrix.forEach((params) => {
+    lockupParamsMatrix.forEach((lockupParams) => {
+      vestingDelegatingTests(params, lockupParams);
+    });
+  });
+  vestingDelegatingErrorTests();
+});
 
-// describe('Testing the unlocked claim tests without delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     unlockedTests(params, true);
-//     unlockedTests(params, false);
-//   });
-//   unlockedErrorTests();
-// })
+describe('Testing the unlocked claim tests without delegation', () => {
+  paramsMatrix.forEach((params) => {
+    unlockedTests(params, true);
+    unlockedTests(params, false);
+  });
+  unlockedErrorTests();
+})
 
-// describe('Testing the locked claim tests without delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     lockupParamsMatrix.forEach((lockupParams) => {
-//       lockedTests(params, lockupParams, true);
-//       lockedTests(params, lockupParams, false);
-//     });
-//   });
-//   lockedErrorTests();
-// })
+describe('Testing the locked claim tests without delegation', () => {
+  paramsMatrix.forEach((params) => {
+    lockupParamsMatrix.forEach((lockupParams) => {
+      lockedTests(params, lockupParams, true);
+      lockedTests(params, lockupParams, false);
+    });
+  });
+  lockedErrorTests();
+})
 
-// describe('Testing the vesting claim tests without delegation', () => {
-//   paramsMatrix.forEach((params) => {
-//     lockupParamsMatrix.forEach((lockupParams) => {
-//       vestingTests(params, lockupParams, true);
-//       vestingTests(params, lockupParams, false);
-//     });
-//   });
-//   vestingErrorTests();
-// })
+describe('Testing the vesting claim tests without delegation', () => {
+  paramsMatrix.forEach((params) => {
+    lockupParamsMatrix.forEach((lockupParams) => {
+      vestingTests(params, lockupParams, true);
+      vestingTests(params, lockupParams, false);
+    });
+  });
+  vestingErrorTests();
+})
 
-// describe('Testing the multi claim tests', () => {
-//   paramsMatrix.forEach((params) => {
-//     multiClaimTests(params);
-//   });
-// })
+describe('Testing the multi claim tests', () => {
+  paramsMatrix.forEach((params) => {
+    multiClaimTests(params);
+  });
+})
 
 describe('Testing the flash loan attack vector', () => {
   flashAttackTests();
